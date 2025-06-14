62de9a338ce2223fcf68a5b4f19449540826bf60
roytheboyyy <roy.mou.safa@gmail.com>
2025-06-14 17:49:49 +0300

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,8 @@
+a = 5
+print("type of a is:" ,type(a))
+b = 1.25
+print("type of b is:" ,type(b))
+c = "python"
+print("type of c is:" ,type(c))
+d= True
+print("type of d is:" ,type(d))
---
+++ activity2.py
@@ -0,0 +1,20 @@
+size = 3.2
+mass = 12
+weak = True
+name = "roy"
+
+print("size :", size)
+print("Datatype:", type(size))
+print("mass :", mass)
+print("Datatype:", type(mass))
+print("weak :", weak)
+print("Datatype:", type(weak))
+print("name :", name)
+print("Datatype:", type(name))
+
+print("\n After type casting...")
+size = int(size)
+print("Datatype:", type(size)) 
+mass=str(mass)
+print(mass)
+print("Datatype:", type(mass))
