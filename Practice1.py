07ca202960478d48afe4e89a6c5a3e77c226dc61
roytheboyyy <roy.mou.safa@gmail.com>
2025-06-21 20:53:05 +0300

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,18 @@
+print("Here are some planets' masses in the solar system!")
+print("earth's weight is 59q.")
+print("and the size is 7926.")
+earth_weight = 5972168101000000000000000
+earth_diameter_inmiles = 7926
+moon_weight = 73500000000000000000000
+moon_diameter = 3476
+q = input("Pick a planet")
+if q == "earth":
+     print(earth_weight + earth_diameter_inmiles)
+elif q == "moon":
+     print(moon_weight + moon_diameter)
+else:
+     print("you can only pick the earth or moon for now! (This update.")
+     
+print("weight and size of earth combined! \n")
+
+
